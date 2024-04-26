a21baac136402a95a72e9afdee7ed9b887dbe5a0
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-04-25 21:59:47 -0300

    att

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function menuShow() {
       "Imagens/icons-header/xbar.svg";
   }
 }
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+ spaceBetwwen: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
