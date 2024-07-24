83c462ee2d29e71e0f4b2896c7afe5a826bdb00a
Samuel Bonini <109481774+samuelbonini@users.noreply.github.com>
2024-07-24 13:18:15 -0300

    Adicionei um SLIDE na parte principal do site (index-main)

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ function menuShow() {
     icon.src = "Imagens/icons-header/xbar.svg";
   }
 }
+
+/*---slides----*/
+var radio = document.querySelector(".manual-btn");
+var cont = 1;
+document.getElementById("radio1").checked = true;
+setInterval(() => {
+  proximaImg();
+}, 5000);
+function proximaImg() {
+  cont++;
+  if (cont > 3) {
+    cont = 1;
+  }
+  document.getElementById("radio" + cont).checked = true;
+}
 /*----------------*/
 
 const initSlider = () => {
